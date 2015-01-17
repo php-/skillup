e2d765f1c010aecf1656f90ab800d8f16706067a
Garcha <george.garcha@gmail.com>
2015-01-18 01:07:45 +0400

    ye

---
+++ SkillUP/src/skillup/garcha/task/car/Car.java
@@ -0,0 +1,72 @@
+
+package skillup.garcha.task.car;
+
+/**
+ * @author skillup
+ */
+public class Car {
+    
+    private static int counter = 0;
+            
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    
+    private String color;
+    private float price; 
+    private String regNumber;
+    
+    
+    public Car(String brand, String model, int year) {
+        this.id = ++counter;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public float getPrice() {
+        return price;
+    }
+    
+}
---
+++ SkillUP/src/skillup/garcha/task/car/CarMarket.java
@@ -0,0 +1,31 @@
+package skillup.garcha.task.car;
+
+/**
+ * @author skillup
+ */
+public class CarMarket {
+    
+    private static Car[] cars;
+    private static String[] brands;
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world's best car Market");
+        
+        
+    }
+    
+    public static void createSomeCars(int count) {
+        cars = new Car[count];
+        
+         
+        Car tmpCar;
+        
+        for(int i = 0; i < count; i++)
+        {
+            tmpCar = CarFactory.createCar("BMW", "D5", 1900);
+            cars[i] = tmpCar;
+        }
+        
+        
+    }
+}
